e9ab6fbac28ded32c35acdf6b06b54b105107340
McHusky <39983522+McHusky@users.noreply.github.com>
2022-11-04 16:22:50 +0100

    Changed StartUp file to check for updates first

--- Rockys Ultimate DL Tool/StartUp.bat
+++ Rockys Ultimate DL Tool/StartUp.bat
@@ -1,7 +1,8 @@
 @ECHO OFF
 
 Pushd "%~dp0"
-powershell -executionpolicy bypass -file functions\main.ps1
+powershell -executionpolicy bypass -file functions\Get-ToolUpdate.ps1
+powershell -executionpolicy bypass -file functions\main.ps1 -windowstyle hidden
 popd
 
 exit
