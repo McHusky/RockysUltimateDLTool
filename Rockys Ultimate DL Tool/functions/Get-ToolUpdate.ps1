2199aa6538ae3dc709421061dc5bbc5c98e6057b
McHusky <39983522+McHusky@users.noreply.github.com>
2022-11-04 16:20:17 +0100

    Added Update script

---
+++ Rockys Ultimate DL Tool/functions/Get-ToolUpdate.ps1
@@ -0,0 +1,35 @@
+$rawGithub = "https://raw.githubusercontent.com/McHusky/RockysUltimateDLTool/main/Rockys%20Ultimate%20DL%20Tool"
+
+$root = ([IO.FileInfo] $MyInvocation.MyCommand.Path).Directory.Parent.FullName
+
+<# OBSOLET - ONLY WORKING OUTSIDE OF SCRIPT
+$exclude =
+@"
+logins.txt
+data
+Get-ToolUpdate.ps1
+"@ -split "`n"
+$toolFiles = (Get-ChildItem -Path $root -Exclude $exclude | Get-ChildItem -File -Recurse).FullName
+#>
+
+$toolFiles = (Get-ChildItem -Path "$root" -File -Recurse).FullName
+
+foreach ($file in $toolFiles)
+{
+   if($file -match "\\data\\" -OR $file -match "logins.txt"){}else{
+      $fileName = $file | Split-Path -Leaf
+      $toolFilesSubpath = $file | foreach {$_.Replace("$root","")}
+      $toolFilesURLs = $toolFilesSubpath | foreach {("$rawGithub"+"$_").Replace("\","/")}
+
+      $contentA = Get-Content $file -Encoding utf8
+      $contentB = Invoke-RestMethod "$toolFilesURLs"
+
+      if (Compare-Object -ReferenceObject $contentA -DifferenceObject ($contentB -replace '\r?\n\z' -split '\r?\n')){
+         Write-Host "`n$([char]0x2715) $fileName`n"
+         Invoke-Webrequest -Uri "$toolFilesURLs" -Outfile "$file"
+      }else{
+         Write-Host "$([char]0x2713) $fileName"
+      }
+   }
+}
+Read-Host "was"
